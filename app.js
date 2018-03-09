eabd003f35c0046afd1b731eeabba6af330f020d
Joe Previte <jjprevite@gmail.com>
2018-03-08 19:40:07 -0700

    Rewrite Twitter package to match example in npm reference

--- app.js
+++ app.js
@@ -2,27 +2,33 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 1337;
-const TwitterPackage = require('twitter');
+const Twitter = require('twitter');
+
+const client = new Twitter{
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+}
 
 const app = express();
-
 
 app.use(bodyParser());
 
 app.post('/sms', (req, res) => {
-    const secret = {
-        consumer_key: process.env.BOT_CONSUMER_KEY,
-        consumer_secret: process.env.BOT_CONSUMER_SECRET,
-        access_token: process.env.BOT_ACCESS_TOKEN,
-        access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-    }
-    const Twitter = new TwitterPackage(secret);
     //get the text message contents 
     let message = req.body.Body;
+    
     console.log(`You received a message that says: ${message}`);
-    Twitter.post('statuses/update', { "status": message }, function (error, tweet, response) {
-        console.log(`Tweet posted successfully! Your tweet said: ${message}`)
-    });
+
+    client.post('statuses/update', { status: message })
+        .then(function (tweet) {
+            console.log(`Tweet posted successfully! Your tweet said: ${tweet}`)
+        })
+        .catch(function (error) {
+            throw error;
+        })
+        
     res.set('Content-Type', 'application/xml');
     res.send('<Response/>')
 }); 
