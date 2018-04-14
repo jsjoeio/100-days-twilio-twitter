20d01dba36594e1c0d4dd6d1aa88a7c2b6affc22
Joe Previte <jjprevite@gmail.com>
2018-04-14 16:41:13 -0700

    test function getting currentDay

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 1337;
 const Twitter = require('twitter');
@@ -21,32 +22,60 @@ app.use(bodyParser());
 
 app.post('/sms', (req, res) => {
   let round = 4;
-  let counter = 0;
-
+  let day = getDayCount();
   if (req.body.From === process.env.PHONE_NUMBER) {
     //get the text message contents
+    getDayCount();
     let text = req.body.Body;
-    let message = `R${round}|D${counter}:\n${text} \n#100DaysOfCode`;
+    let message = `R${round}|D${day}:\n${text} \n#100DaysOfCode`;
 
     console.log(`You received a message that says: ${message}`);
 
-    twitterClient
-      .post('statuses/update', {
-        status: message
-      })
-      .then(function(tweet) {
-        console.log(`Tweet posted successfully! Your tweet said: ${tweet}`);
-      })
-      .catch(function(error) {
-        console.log(
-          `Uh oh...Looks like we've got an error here: ${JSON.stringify(error)}`
-        );
-      });
+    // twitterClient
+    //   .post('statuses/update', {
+    //     status: message
+    //   })
+    //   .then(function(tweet) {
+    //     console.log(`Tweet posted successfully! Your tweet said: ${tweet}`);
+    //   })
+    //   .catch(function(error) {
+    //     console.log(
+    //       `Uh oh...Looks like we've got an error here: ${JSON.stringify(error)}`
+    //     );
+    //   });
 
     res.set('Content-Type', 'application/xml');
     res.send('<Response/>');
   }
 });
+
+//function 1
+//get the day...
+
+function getDayCount() {
+  const options = {
+    method: 'GET',
+    url: 'https://dm-meeting-app.firebaseio.com/roundFour.json',
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  };
+
+  const currentDay = request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    let parsed = JSON.parse(body);
+    let arrOfDayObjects = [];
+    for (let dayObject in parsed) {
+      arrOfDayObjects.push(parsed[dayObject]);
+    }
+    let currentDayObject = arrOfDayObjects[arrOfDayObjects.length - 1];
+    return currentDayObject.day;
+  });
+  console.log('outside the request, currentDay is ', currentDay);
+}
+
+//function 2
+//post to the DB...
 
 http.createServer(app).listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
